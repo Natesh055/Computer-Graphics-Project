627d154d8adafc3e2735ad419d864bac43707ef6
Natesh055 <singhnatesh50@gmail.com>
2025-01-20 15:35:58 +0530

    Initial Commit

---
+++ glutproj/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  glutproj
+//
+//  Created by Vijay Singh Pundir on 20/01/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

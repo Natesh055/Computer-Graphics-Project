66f9f5ac98e083e049f819147f9c08886bc19f06
Natesh055 <singhnatesh50@gmail.com>
2025-05-03 10:44:48 +0530

    GoalKeeper Added in front of Goal need to change the color of the goalkeeper's left hand

--- glutproj/main.cpp
+++ glutproj/main.cpp
@@ -1,11 +1,72 @@
 #include <GL/glut.h>
-#include <cmath>
+#include "math.h"
+#define DEG2RAD 3.14159/180.0
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
 GLUquadric* quad;
 
 float ballZ = 10.0f;
 float ballSpeed = -0.05f;
 bool goingForward = true;
+
+void DrawEllipse(float radiusX, float radiusY, int posx, int posy) {
+    int i;
+    glBegin(GL_POLYGON);
+    for (i = 0; i < 360; i++) {
+        float rad = i * DEG2RAD;
+        glVertex2f(posx + cos(rad) * radiusX,
+                   posy + sin(rad) * radiusY);
+    }
+    glEnd();
+}void humanBody() {
+    // Set skin color once for all body parts except vessels
+    glColor3f(1.0f, 0.87f, 0.77f); // Skin color
+
+    // --- Legs ---
+    glPushMatrix();
+    glRotatef(-10, 0, 0, 1);
+    DrawEllipse(10, 50, 0, 0); // Left leg
+    glPopMatrix();
+
+    glPushMatrix();
+    glRotatef(10, 0, 0, 1);
+    DrawEllipse(10, 50, 25, -7); // Right leg
+    glPopMatrix();
+
+    // --- Torso ---
+    DrawEllipse(20, 50, 12, 70); // Center torso
+
+    // --- Arms ---
+    glPushMatrix();
+    glRotatef(60, 0, 0, 1);
+    DrawEllipse(40, 10, 60, 55); // Left arm
+    glPopMatrix();
+
+    glPushMatrix();
+    glRotatef(-60, 0, 0, 1);
+    DrawEllipse(40, 10, -50, 75); // Right arm
+    glPopMatrix();
+
+    // --- Head ---
+    DrawEllipse(15, 25, 12, 135); // Head
+
+    // --- Blood Vessels ---
+    glPushMatrix();
+    glColor3f(1.0f, 0.0f, 0.0f); // Red for vessels
+    glBegin(GL_LINES);
+    glVertex2f(12, 30); glVertex2f(12, 120);        // Center vertical
+    glVertex2f(12, 118); glVertex2f(-8, 90);        // Left upper
+    glVertex2f(-8, 90); glVertex2f(-30, 60);
+    glVertex2f(12, 118); glVertex2f(50, 65);        // Right upper
+    glVertex2f(12, 35); glVertex2f(25, 10);         // Right lower
+    glVertex2f(25, 10); glVertex2f(30, -20);
+    glVertex2f(12, 35); glVertex2f(2, 10);          // Left lower
+    glVertex2f(2, 10); glVertex2f(-5, -30);
+    glEnd();
+    glPopMatrix();
+}
 
 void init() {
     glEnable(GL_DEPTH_TEST);
@@ -16,7 +77,7 @@ void init() {
     GLfloat lightPos[] = {10.0f, 10.0f, 10.0f, 1.0f};
     glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
 
-    glClearColor(0.2f, 0.7f, 0.3f, 1.0f); // Green field background
+    glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
 
     glMatrixMode(GL_PROJECTION);
     gluPerspective(60.0, 1.0, 1.0, 100.0);
@@ -45,7 +106,7 @@ void drawGoal() {
 }
 
 void drawGround() {
-    glColor3f(0.1f, 0.5f, 0.1f);
+    glColor3f(0.2f, 0.8f, 0.2f);
     glBegin(GL_QUADS);
     glVertex3f(-20, 0, -10);
     glVertex3f(20, 0, -10);
@@ -53,48 +114,6 @@ void drawGround() {
     glVertex3f(-20, 0, 20);
     glEnd();
 }
-//
-//void drawNet() {
-//    glColor3f(1.0f, 1.0f, 1.0f);
-//    glBegin(GL_LINES);
-//
-//    float width = 7.32f;
-//    float halfWidth = width / 2.0f;
-//    float height = 4.0f;
-//    float depth = 2.5f;
-//    float spacing = 0.15f;
-//
-//    // Vertical strings (X-Z)
-//    for (float x = -halfWidth; x <= halfWidth; x += spacing) {
-//        for (float y = 0.0f; y <= height; y += spacing) {
-//            float zFront = 0.0f;
-//            float curve = (1.0f - powf(x / halfWidth, 2)) * depth; // Parabolic curve
-//            glVertex3f(x, y, zFront);
-//            glVertex3f(x, y, curve);
-//        }
-//    }
-//
-//    // Horizontal strings (X across Z)
-//    for (float z = 0.0f; z <= depth; z += spacing) {
-//        for (float y = 0.0f; y <= height; y += spacing) {
-//            float xStart = -halfWidth * (1.0f - z / depth);
-//            float xEnd = halfWidth * (1.0f - z / depth);
-//            glVertex3f(xStart, y, z);
-//            glVertex3f(xEnd, y, z);
-//        }
-//    }
-//
-//    // Depth strings (Y-Z)
-//    for (float x = -halfWidth; x <= halfWidth; x += spacing) {
-//        for (float z = 0.0f; z <= depth; z += spacing) {
-//            float curve = (1.0f - powf(x / halfWidth, 2)) * z;
-//            glVertex3f(x, 0.0f, curve);
-//            glVertex3f(x, height, curve);
-//        }
-//    }
-//
-//    glEnd();
-//}
 
 void drawPenaltyBox() {
     glColor3f(1.0f, 1.0f, 1.0f);
@@ -137,14 +156,18 @@ void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
-    // Front view camera
-    gluLookAt(0.0, 3.0, 10.0,
-              0.0, 2.0, 0.0,
-              0.0, 1.0, 0.0);
+    gluLookAt(0.0, 3.0, 10.0, 0.0, 2.0, 0.0, 0.0, 1.0, 0.0);
 
     drawGround();
     drawPenaltyBox();
     drawGoal();
+
+    // Draw human body as goalkeeper
+    glPushMatrix();
+    glTranslatef(0.0f, 0.05f, 0.3f);  // X=center, Y=on ground, Z=slightly inside goal
+    glScalef(0.015f, 0.015f, 0.015f); // Scale to fit inside goal
+    humanBody();
+    glPopMatrix();
 
     drawBall();
 
